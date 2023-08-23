75789520cef9de98bae8209bd4c3ded23a0f36fa
GIBS <83280831+BGGBGIBS@users.noreply.github.com>
2023-08-23 19:46:39 +0200

    maj

--- Persistence/Customers/CustomerConfiguration.cs
+++ Persistence/Customers/CustomerConfiguration.cs
@@ -1,7 +1,8 @@
 using TestShop.Domain.Customers;
 
 namespace TestShop.Persistence.Customers{
-    public class CustomerConfiguration : EntityTypeConfiguration<Customer>{
+    public class CustomerConfiguration : EntityTypeConfiguration<Customer>
+    {
         public CustomerConfiguration(){
             HasKey(p=> p.Id);
             PropertyAccessors(p=> p.Name).IsRequired().HasMaxLength(50);
--- Persistence/Products/ProductConfiguration.cs
+++ Persistence/Products/ProductConfiguration.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Data.Entity.ModelConfiguration;
 using System.Reflection.Metadata;
 using TestShop.Domain.Products;
 
